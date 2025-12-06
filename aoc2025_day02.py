a46918690a2d271777b647026ce6c81ee6df92dc
aconconi <>
2025-12-06 19:16:18 +0100

    type fix

--- aoc2025_day02.py
+++ aoc2025_day02.py
@@ -9,39 +9,40 @@ import pytest
 
 
 def parse_input(file_name: str) -> list[tuple[int, int]]:
-     with open(file_name, "r", encoding="ascii") as data_file:
-         return [
-             tuple(map(int, id_range.split("-")))
-             for id_range in data_file.read().strip().split(",")
-         ]
+    with open(file_name, "r", encoding="ascii") as data_file:
+        ranges: list[tuple[int, int]] = []
+        for id_range in data_file.read().strip().split(","):
+            low_str, high_str = id_range.split("-")
+            ranges.append((int(low_str), int(high_str)))
+        return ranges
 
 
 def solve(id_ranges: list[tuple[int, int]], pattern: str) -> int:
-     return sum(
-         sum(id for id in range(low, high + 1) if re.fullmatch(pattern, str(id)))
-         for low, high in id_ranges
-     )
+    return sum(
+        sum(id for id in range(low, high + 1) if re.fullmatch(pattern, str(id)))
+        for low, high in id_ranges
+    )
 
 
 def day02_part1(id_ranges: list[tuple[int, int]]) -> int:
-     return solve(id_ranges, r"(\d+)\1")
+    return solve(id_ranges, r"(\d+)\1")
 
 
 def day02_part2(id_ranges: list[tuple[int, int]]) -> int:
-     return solve(id_ranges, r"(\d+)\1{1,}")
+    return solve(id_ranges, r"(\d+)\1{1,}")
 
 
 @pytest.fixture(autouse=True, name="test_data")
 def fixture_test_data() -> list[tuple[int, int]]:
-     return parse_input("data/day02_test.txt")
+    return parse_input("data/day02_test.txt")
 
 
 def test_day02_part1(test_data: list[tuple[int, int]]) -> None:
-     assert day02_part1(test_data) == 1227775554
+    assert day02_part1(test_data) == 1227775554
 
 
 def test_day02_part2(test_data: list[tuple[int, int]]) -> None:
-     assert day02_part2(test_data) == 4174379265
+    assert day02_part2(test_data) == 4174379265
 
 
 if __name__ == "__main__":
